26c7ee4101bc0087b39e1ecb4596d5387f145204
JsFn99 <fnine2015@gmail.com>
2024-11-27 14:46:01 +0100

    home sidebar added

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -4,14 +4,14 @@ class Background extends StatelessWidget {
   final Widget child;
 
   const Background({
-    Key? key,
+    super.key,
     required this.child,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
+    return SizedBox(
       width: double.infinity,
       height: size.height,
       child: Stack(
@@ -21,40 +21,40 @@ class Background extends StatelessWidget {
             top: 0,
             right: 0,
             child: Image.asset(
-              "assets/images/top1.png",
-              width: size.width
+                "assets/images/top1.png",
+                width: size.width
             ),
           ),
           Positioned(
             top: 0,
             right: 0,
             child: Image.asset(
-              "assets/images/top2.png",
-              width: size.width
+                "assets/images/top2.png",
+                width: size.width
             ),
           ),
           Positioned(
             top: 50,
             right: 30,
             child: Image.asset(
-              "assets/images/main.png",
-              width: size.width * 0.35
+                "assets/images/logo.png",
+                width: size.width * 0.25
             ),
           ),
           Positioned(
             bottom: 0,
             right: 0,
             child: Image.asset(
-              "assets/images/bottom1.png",
-              width: size.width
+                "assets/images/bottom1.png",
+                width: size.width
             ),
           ),
           Positioned(
             bottom: 0,
             right: 0,
             child: Image.asset(
-              "assets/images/bottom2.png",
-              width: size.width
+                "assets/images/bottom2.png",
+                width: size.width
             ),
           ),
           child
